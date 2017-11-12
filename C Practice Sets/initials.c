d67905c95db9e349705cd3f9d087cc3b73810078
ampourgh <ampourgh@gmail.com>
2017-11-11 23:05:55 -0600

    Added comments and formating to initials.c

--- C Practice Sets/initials.c
+++ C Practice Sets/initials.c
@@ -3,40 +3,42 @@
 #include <cs50.h>
 #include <string.h>
 
-int main(void)
-{
-printf("Name: ");
-string y = get_string();
-int z = strlen(y);
+int main(void) {
+    // input after compiler is running
+    printf("Name: ");
+    string y = get_string();
+
+    // used to loop through with incrementor
+    int z = strlen(y);
 
 
-// while incrementer is less than the string length
-for ( int a = 0; a < z; a++)
-    {
+    // while incrementer is less than the string length
+    for (int a = 0; a < z; a++) {
 
-        if(a == 0)
-        {
-            printf("%c", toupper(y[a]));
-            a++;
+            // If first letter, capitalize and increment
+            if(a == 0) {
+                printf("%c", toupper(y[a]));
+                a++;
+            }
+
+            // After first letter, increment until there's a space
+            do {
+                a++;
+            }
+            while(y[a] != ' ');
+
+            // increment on the space
+            if(y[a] == ' ') {
+                a++;
+            }
+
+            // if not a space, capitalize the letter
+            if(y[a] != ' ') {
+                printf("%c", toupper(y[a]));
+            }
+
         }
 
-        do
-        {
-            a++;
-        }
-        while(y[a] != ' ');
-
-        if(y[a] == ' ')
-        {
-            a++;
-        }
-
-        if(y[a] != ' ')
-        {
-            printf("%c", toupper(y[a]));
-        }
-
-    }
-
+    // end of line
     printf("\n");
 }
